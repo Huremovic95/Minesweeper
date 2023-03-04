a5cfc58f325ad8f0ae34a2c5d75fb8c07b519e03
Huremovic95 <emirhuric@icloud.com>
2023-03-04 21:14:33 +0000

    Add cd function that checks if user input hit a mine

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import random
 EMPTY = 0
 MINE = 9
 UNKNOWN = -1
+FLAG = 1
 
 # Solution grid that the user can not see
 grid = [
@@ -70,7 +71,7 @@ def show_grid():
     https://www.youtube.com/watch?v=XTT8mXwIGpQ
     https://github.com/wynand1004/Projects/blob/master/Minesweeper/minesweeper.py
     """
-    symbols = {-2: "F", -1: "-"}
+    symbols = {1: "F", -1: "-"}
     for row in range(0, 7):
         for col in range(0, 7):
             value = grid_display[row][col]
@@ -83,18 +84,22 @@ def show_grid():
 
 
 def ask_coordinates():
-    
+    """
+    Ask the user if they want to put a flag goes to set flag function
+    or otherwise but a row and a column. Because user experience asks
+    between 1 and 7 and puts -1 to the input after that checks if the input
+    is between 0 and 6. Passes row and column to cell_activated function. 
+    """
     row = input("Press f to set a flag or guess a row between 1 and 7: ")
     row = row.lower()
     if row == "f":
         # flag funtion
         print("setting a flag")
     else:
-        row = int(row)
-        print(row)
+        row = int(row) -1
 
     try:
-        if row > 7 or row < 1:
+        if row > 6 or row < 0:
             print("Please choose a number between 1 and 7")
             return False
 
@@ -103,10 +108,10 @@ def ask_coordinates():
         print(f"Invalid data: {e}, please try again.")
         return False
 
-    column = int(input("guess a column between 1 and 7: "))
+    column = int(input("guess a column between 1 and 7: ")) -1
 
     try:
-        if column > 7 or column < 1:
+        if column > 6 or column < 0:
             print("Please choose a number between 1 and 7")
             return False
 
@@ -114,6 +119,16 @@ def ask_coordinates():
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.")
         return False
+
+    cell_activated(row, column)
+
+
+def cell_activated(row, column):
+    """
+    Checks if the user row and column input hit a mine.
+    """
+    if grid[row][column] == MINE:
+        print("Gameover! You hit a mine!!!")
 
 
 def main():
