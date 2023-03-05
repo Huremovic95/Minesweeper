63acfe5a334861449c5a1b0bb1dd84c7f420db05
Huremovic95 <emirhuric@icloud.com>
2023-03-05 19:26:25 +0000

    Add while loops for input when wrong answer keeps asking the question

--- run.py
+++ run.py
@@ -33,20 +33,24 @@ def ask_mines():
     """
     converts all strings into integers gives the user an option
     to choose the amount of mines. if the amount of mines is 
-    not between 5 and 20 the user will be asked again 
-    and Raises ValueError if strings cannot be converted into int.
+    not between 5 and 15 the user will be asked again and 
+    raises ValueError if strings cannot be converted into int.
     """
-    num_mines = int(input("Choose how many mines you want. choose a number between 5 and 20: \n"))
+    while True:    
+        try:
+            num_mines = int(input("Choose how many mines you want. choose a number between 5 and 15: \n"))     
 
-    try:
-        if num_mines > 20 or num_mines < 5:
-            print("Please choose a number between 5 and 20")
-            return False
+        # copied from lovesandwiches project
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.")
+            continue
 
-    # copied from lovesandwiches project
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.")
-        return False
+        if num_mines > 15 or num_mines < 5:
+            print("Please choose a number between 5 and 15")
+            continue
+        else:
+            # num_mines successfully parsed, and we're happy with its value.
+            break
 
     return add_mines(num_mines)
 
@@ -57,12 +61,15 @@ def add_mines(mines):
     number of mines to the grid. also checks if the
     space is not already occupied by a mine.
     """
-    for num in range(mines + 1):
-        x = random.randint(0, 6)  # x axis/horizontal
-        y = random.randint(0, 6)  # y axis/vertical
-        if grid[x][y] == 0:
-            grid[x][y] = MINE  # MINE = 9
-
+    counter = 0
+    if counter < mines:
+        for counter in range(mines):
+            x = random.randint(0, 6)  # x axis/horizontal
+            y = random.randint(0, 6)  # y axis/vertical
+            if grid[x][y] == 0:
+                grid[x][y] = MINE  # MINE = 9
+                counter = counter + 1
+ 
 
 def show_grid():
     """
@@ -74,7 +81,7 @@ def show_grid():
     symbols = {1: "F", -1: "-"}
     for row in range(0, 7):
         for col in range(0, 7):
-            value = grid_display[row][col]
+            value = grid[row][col]
             if value in symbols:
                 symbol = symbols[value]
             else:
@@ -85,40 +92,49 @@ def show_grid():
 
 def ask_coordinates():
     """
-    Ask the user if they want to put a flag goes to set flag function
+    Ask the user if they want to put a flag goes to set_flag function
     or otherwise but a row and a column. Because user experience asks
     between 1 and 7 and puts -1 to the input after that checks if the input
     is between 0 and 6. Passes row and column to cell_activated function. 
     """
-    row = input("Press f to set a flag or guess a row between 1 and 7: ")
-    row = row.lower()
-    if row == "f":
-        # flag funtion
-        print("setting a flag")
-    else:
-        row = int(row) -1
+    while True:
+        try:
+            row = input("Press f to set a flag or guess a row between 1 and 7: ")
+            row = row.lower()
+            if row == "f":
+                # flag funtion
+                print("setting a flag")
+                break
+            else:
+                row = int(row)-1
 
-    try:
-        if row > 6 or row < 0:
-            print("Please choose a number between 1 and 7")
-            return False
+            if row > 6 or row < 0:
+                print("Please choose a number between 1 and 7")
+                continue
+                    
+            # copied from lovesandwiches project
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.")
+            continue
 
-    # copied from lovesandwiches project
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.")
-        return False
+        else:
+            break
+        
+    while True:
+        try:
+            column = int(input("guess a column between 1 and 7: "))-1
+            
+            if column > 6 or column < 0:
+                print("Please choose a number between 1 and 7")
+                continue
+                
+        # copied from lovesandwiches project
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.")
+            continue
 
-    column = int(input("guess a column between 1 and 7: ")) -1
-
-    try:
-        if column > 6 or column < 0:
-            print("Please choose a number between 1 and 7")
-            return False
-
-    # copied from lovesandwiches project
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.")
-        return False
+        else:
+            break
 
     cell_activated(row, column)
 
